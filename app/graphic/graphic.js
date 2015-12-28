d4087a50b8ab9b1dc6203d100233e432c966e1df
Andres Abril <andres.abril@gmail.com>
2015-12-28 16:58:42 -0500

    Created 2nd type of chart with bottom labels - Simplified the html code - Better structure for the chart JavaScript object

--- app/graphic/graphic.js
+++ app/graphic/graphic.js
@@ -2,64 +2,116 @@
 
     app.controller('GraphicController', ['$scope', function ($scope) {
 
-        var init = function() {
-			console.log('testeando: ',$scope.test);
-			
-			$scope.labels = ["Download Sales", "In-Store Sales"];
-			$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-			//$scope.data = [800, 200];
+        var init = function() {			
 		};
         init();
     }]);
 
-	app.directive('graphic', function() {
-		var green = {hex:'#76ba0a', name:'green'};
-		var orange = {hex:'#ffb702', name:'orange'};
-		var darkGray = {hex:'#3b3b3b', name:'darkGray'};
+	app.directive('donut', function() {
+		//Define theme colors
+		//These theme colors are objects with a main color, a name, and a secondary color
+		var green = {hex:'#76ba0a', name:'green', secondaryHex:'#eeeeee'};
+		var blue = {hex:'#31a9ef', name:'blue', secondaryHex:'#eeeeee'};
+		var orange = {hex:'#ffb702', name:'orange', secondaryHex:'#eeeeee'};
+		var darkGray = {hex:'#3b3b3b', name:'darkGray', secondaryHex:'#eeeeee'};
+		
 		var themeColor = '';
+		var themeNames = ['green', 'orange', 'darkGray', 'blue'];
 		var secondaryColor = '';
+		var themeArray = [green, orange, darkGray, blue];
+		
+		var linkFunction = function(scope, element, attrs) {
+			scope.$watch('chart', function(newValue,oldValue){
+				
+				if(	scope.chart.css.theme != 'green' &&
+					scope.chart.css.theme != 'orange'&&
+					scope.chart.css.theme != 'darkGray' &&
+					scope.chart.css.theme != 'blue')
+				{
+					scope.chart.css.theme = 'green';
+				}				
+				
+				themeColor =
+								scope.chart.css.theme == "green" ? green : 
+								scope.chart.css.theme == "orange" ? orange :
+								scope.chart.css.theme == "blue" ? blue :
+								scope.chart.css.theme == "darkGray" ? darkGray : green;
+													
+				scope.cardClass = {};
+				scope.bgColor = {};
+				scope.textColor = {};
+				scope.textColor[themeColor.name+'Text'] = true;
+				scope.bgColor[themeColor.name+'Bg'] = true;
+				scope.cardClass.shadow = scope.chart.css.shadow;
+				scope.colours = scope.chart.chartData.labels.length > 2 ? scope.chart.chartData.colors : [themeColor.hex, themeColor.secondaryHex];
+				scope.data = scope.chart.chartData.data;
+				scope.labels = scope.chart.chartData.labels;
+				scope.type = scope.chart.css.type;
+			});
+		};
 		
 		return {
 			restrict: 'E',
 			templateUrl: 'graphic/graphic.tpl.html',
 			replace: true,
 			scope: {
-				options : '=',
-				bg : '=',
-				cardname : '=',
-				bottomtitle : '=',
-				bottomsubtitle : '=',
-				datai : '=',
+				chart : '=?'
 			},
 			controller : "GraphicController",
 			
-			link: function(scope, element, attrs) {
-				scope.$watch('options', function(newValue,oldValue){
-					
-					if(	scope.options.css.theme != 'green' &&
-						scope.options.css.theme != 'orange'&&
-						scope.options.css.theme != 'darkGray')
-					{
-						scope.options.css.theme = 'green';
-					}
-					themeColor =
-									scope.options.css.theme == "green" ? green.name : 
-									scope.options.css.theme == "orange" ? orange.name :
-									scope.options.css.theme == "darkGray" ? darkGray.name : green.name;
-					
-					secondaryColor = '#eeeeee';
-										
-					scope.cardClass = {};
-					scope.bgColor = {};
-					scope.textColor = {};
-					scope.textColor[themeColor+'Text'] = true;
-					scope.bgColor[themeColor+'Bg'] = true;
-					scope.cardClass.shadow = scope.options.css.shadow;
-					scope.colours = [themeColor, secondaryColor,themeColor, secondaryColor];
-					scope.datax = scope.datai;
-					console.log('Datax: ',scope.datax);
-				});
-			}
+			link: linkFunction
+		};
+	});
+	app.directive('bars', function() {
+		//Define theme colors
+		//These theme colors are objects with a main color, a name, and a secondary color
+		var green = {hex:'#76ba0a', name:'green', secondaryHex:'#eeeeee'};
+		var orange = {hex:'#ffb702', name:'orange', secondaryHex:'#eeeeee'};
+		var darkGray = {hex:'#3b3b3b', name:'darkGray', secondaryHex:'#eeeeee'};
+		
+		var themeColor = '';
+		var secondaryColor = '';
+		var themeArray = [green,orange,darkGray];
+		
+		var linkFunction = function(scope, element, attrs) {
+			scope.$watch('chart', function(newValue,oldValue){
+				
+				if(	scope.chart.css.theme != 'green' &&
+					scope.chart.css.theme != 'orange'&&
+					scope.chart.css.theme != 'darkGray')
+				{
+					scope.chart.css.theme = 'green';
+				}
+				themeColor =
+								scope.chart.css.theme == "green" ? green : 
+								scope.chart.css.theme == "orange" ? orange :
+								scope.chart.css.theme == "darkGray" ? darkGray : green;
+				
+				secondaryColor = '#eeeeee';
+									
+				scope.cardClass = {};
+				scope.bgColor = {};
+				scope.textColor = {};
+				scope.textColor[themeColor.name+'Text'] = true;
+				scope.bgColor[themeColor.name+'Bg'] = true;
+				scope.cardClass.shadow = scope.chart.css.shadow;
+				scope.colours = scope.chart.chartData.labels.length > 2 ? scope.chart.chartData.colors : [themeColor.hex, themeColor.secondaryHex];
+				scope.data = scope.chart.chartData.data;
+				scope.labels = scope.chart.chartData.labels;
+				scope.type = scope.chart.css.type;
+			});
+		};
+		
+		return {
+			restrict: 'E',
+			templateUrl: 'graphic/graphic.tpl.html',
+			replace: true,
+			scope: {
+				chart : '=?'
+			},
+			controller : "GraphicController",
+			
+			link: linkFunction
 		};
 	});
 
--- app/home/home.js
+++ app/home/home.js
@@ -1,26 +1,17 @@
-/**
- * Each section of the site has its own module. It probably also has
- * submodules, though this boilerplate is too simple to demonstrate it. Within
- * 'src/app/home', however, could exist several additional folders representing
- * additional modules that would then be listed as dependencies of this one.
- * For example, a 'note' section could have the submodules 'note.create',
- * 'note.delete', 'note.edit', etc.
- *
- * Regardless, so long as dependencies are managed correctly, the build process
- * will automatically take take of the rest.
- */
 (function(app) {
 
     app.config(function ($stateProvider) {
         $stateProvider.state('bars', {
             url: '/bars',
             views: {
-                "main": {
-                    controller: 'HomeController',
-                    templateUrl: 'home/home.tpl.html'
-                }
+				"main": {
+					controller: 'HomeController',
+					templateUrl: 'home/home.tpl.html'
+				}
             },
-            data:{ pageTitle: 'Bars' }
+            data: { 
+				pageTitle: 'Bars' 
+			}
         });
     });
 
@@ -29,87 +20,81 @@
     app.controller('HomeController', function ($scope) {
 
         var init = function() {
-            // A definitive place to put everything that needs to run when the controller starts. Avoid
-            //  writing any code outside of this function that executes immediately.
+           
         };
-		$scope.animation = false;
-		$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-		$scope.series = ['Series A', 'Series B', 'Series C'];
-		$scope.datax = [65, 59, 80, 81, 56, 55, 40];
 		
-		$scope.options1 = {
+		$scope.chart1 = {
 			css : {
-				type:"orangeCard", 
 				shadow:true, 
 				theme:"orange", 
-				img:"http://yara.co.uk/images/430-43529CORRECT%20%20Banner%20Image%20Environment.png",
+				img:"http://wcdn2.dataknet.com/static/resources/icons/set109/3b9a43ea.png",
+				type: 'multi'
 			},
-			chart : {
+			chartOptions : {
 				percentageInnerCutout:75,
-				animation: false,
-				animateScale : false,
-				animationSteps : 100,
-				//animationEasing : "",
-				onAnimationProgress: function(){ 
-					//console.log('Animating dude');
-				},
-				onAnimationComplete: function(){
-					
-				},
-				showTooltips : false,
-				multiTooltipTemplate: "<%= value %>",
-				tooltipFontColor: "#fff"
+				animation: true,
+				animateScale : true,
+				showTooltips : false
+			},
+			chartData : {
+				data : [20, 10, 40, 30, 56, 55, 40],
+				labels : ['Series A', 'Series B'],
+				colors : ['#dfdfdf','#a0a0a0', '#ffb702', '#ff0A00']
+			},
+			text : {
+				title : 'Lost Energy Costs',
+				bottomTitle : '$213,557',
+				bottomSubtitle : 'GOAL $300,000'
 			}
 		};
 		
-		$scope.options2 = {
+		$scope.chart2 = {
 			css : {
-				type:"greenCard", 
 				shadow:true, 
 				theme:"green" , 
-				img:"http://www.ei1.com/images/ih_icon.png"
+				img:"http://www.ei1.com/images/ih_icon.png",
+				type: 'multi'
 			},
-			chart : {
+			chartOptions : {
 				percentageInnerCutout:75,
-				animation: false,
-				animateScale : false,
-				animationSteps : 100,
-				//animationEasing : "",
-				onAnimationProgress: function(){ 
-					//console.log('Animating dude');
-				},
-				onAnimationComplete: function(){
-					
-				},
-				showTooltips : false,
-				multiTooltipTemplate: "<%= value %>",
-				tooltipFontColor: "#fff"
+				animation: true,
+				animateScale : true,
+				showTooltips : false
+			},
+			chartData : {
+				data : [20, 10, 40, 30, 56, 55, 40],
+				labels : ['Series A', 'Series B', 'Series C', 'Series D'],
+				colors : ['#dfdfdf','#a0a0a0', '#ffb702', '#ff0A00']
+			},
+			text : {
+				title : 'Trap Status',
+				bottomTitle : '150 TRAPS',
+				bottomSubtitle : 'GOAL N/A'
 			}
 			
 		};
 		
-		$scope.options3 = {
+		$scope.chart3 = {
 			css : {
-				type:"darkGrayCard", 
 				shadow:true, 
-				theme:"darkGray" , 
+				theme:"blue" , 
 				img:"http://www.e2cbms.com/images/1_e2c/Breathe_ActionIcons_v2-11.png"
 			},
-			chart : {
+			chartOptions : {
 				percentageInnerCutout:75,
-				animation: false,
-				animateScale : false,
-				animationSteps : 100,
-				//animationEasing : "",
-				onAnimationProgress: function(){ 
-					//console.log('Animating dude');
-				},
-				onAnimationComplete: function(){
-					
-				},
-				showTooltips : false,
-				multiTooltipTemplate: "<%= value %>",
-				tooltipFontColor: "#fff"
+				animation: true,
+				animateScale : true,
+				showTooltips : false
+			},
+			chartData : {
+				data : [20, 10, 40, 30, 56, 55, 40],
+				labels : ['Series A', 'Series B'],
+				colors : ['#dfdfdf','#a0a0a0', '#ffb702', '#ff0A00']
+			},
+			text : {
+				title : 'CO2 Emissions',
+				bottomTitle : '455 LB/HR',
+				bottomSubtitle : 'GOAL 500 LB/HR'
 			}
 		};
 
